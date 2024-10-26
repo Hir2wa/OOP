1b37dab0b1298673380cb3f07f0f5632dd840ced
Hir2wa <alainfabricehirwa@gmail.com>
2024-10-26 20:39:59 +0300

    Add files via upload

---
+++ student.java
@@ -0,0 +1,11 @@
+public class student {
+
+   public static void main(String[] args) {
+    int  age=5; //initialisation
+   
+    // start by data type then variables 
+    System.out.println("my age is "+age);
+   }
+        
+    }
+ 
