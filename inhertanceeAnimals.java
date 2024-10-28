5b17e95e8cb5f4a64d72daa6cd9f387ced36e832
Hir2wa <alainfabricehirwa@gmail.com>
2024-10-28 21:41:26 +0300

    Add files via upload

---
+++ inhertanceeAnimals.java
@@ -0,0 +1,24 @@
+public  class inhertanceeAnimals {
+    
+String type;
+String size;
+String weigth;
+
+public void moVe(){
+    System.out.println("the dog is moving");
+}
+public void makeNoise(){
+ 
+System.out.println(    );
+
+}
+
+
+}
+//class  into  parent-child 
+//code resubility 
+//we must  have  parent  and  child   which  help child to inheret  to  parent 
+//  for us  to use  inhertance it is  code resuability
+//we say  inhertance onlty  when we have  child and parents 
+//which  help  a  child to get  everthing  from child 
+//class  are attribute and  methods 
