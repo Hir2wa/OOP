1a2feb189e23e6c78a4445e502c1ec4d2dfc1226
Hir2wa <alainfabricehirwa@gmail.com>
2024-10-28 21:42:12 +0300

    Add files via upload

---
+++ inhertannceeDog.java
@@ -0,0 +1,15 @@
+public class inhertannceeDog extends inhertanceeAnimals {
+      String earShape;
+      String tailShape;
+
+
+      public void bulk(){
+     System.out.println("wuuuuuuuuuuuuuuuuuuullllllllllllllllllllllkkkk");
+      }
+      public void run(){
+       System.out.println("runnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn");
+      }
+      public  void walk(){
+   System.out.println("waaaaaaaaaaaaaalllllllllllllllllllkkkkkkkkkkk");
+      }
+}
