dd82bcd572fea704ef5215c3a5083f9300a363a2
Hir2wa <alainfabricehirwa@gmail.com>
2024-10-30 15:06:28 +0300

    Add files via upload

---
+++ Mainnn.java
@@ -0,0 +1,12 @@
+public class Mainnn {
+    
+    public static void main(String[] args) {
+        
+      inhertannceeDog dog = new inhertannceeDog();
+    /*    dog.size = "hhhhhhhhhhhh";
+        inhertanceeAnimals animals = new inhertanceeAnimals();
+      */   
+
+
+    }
+}
