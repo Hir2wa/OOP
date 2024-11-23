24b1961582293ae39613bf6002025ca2754674ea
Hirwa  Alain Fabrice <alainfabricehirwa@gmail.com>
2024-11-23 21:28:53 +0200

    calculator.java

--- calculator.java
+++ calculator.java
@@ -2,7 +2,7 @@ public class calculator {
     public static void main(String[] args) {
        int c=19799;
        int g=20;
-       int h=25000000;
+       int h=2500000;
        double d= (c+g)*h/4-h;
 
       if (d>1000) {
