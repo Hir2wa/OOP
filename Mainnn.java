09bbbf5d1d9ea32e52b8ebfe6885397cdff9bf07
Hirwa  Alain Fabrice <alainfabricehirwa@gmail.com>
2024-11-23 21:24:28 +0200

    Mainnn.java

--- Mainnn.java
+++ Mainnn.java
@@ -3,7 +3,7 @@ public class Mainnn {
     public static void main(String[] args) {
         
       inhertannceeDog dog = new inhertannceeDog();
-    /*    dog.size = "hhhhhhhhhhhh";
+    /*    dog.size = "hhhhhhhhhh";
         inhertanceeAnimals animals = new inhertanceeAnimals();
       */   
 
